61ee9467afc0344381513a4b0c8dd052b875dea7
Ben Lickly <blickly@eecs>
2009-08-07 15:59:27 -0700

    Revision 1 of my Hint-peeking plugin

--- Hint-peeking.py
+++ Hint-peeking.py
@@ -1,38 +1,38 @@
 # -*- coding: utf-8 -*-
-
-#######################################################
-#  Two-step answer plugin
-#
-# This plugin allows peeking at one of the fields in
-# a flashcard before seeing the answer.
-#
-# Authors: Ben Lickly <blickly at berkeley dot edu>
-#          Eric Pignet <eric at erixpage dot com>
+# Author:  Ben Lickly <blickly at berkeley dot edu>
+#         (based on 'Two-step answer' plugin by Eric Pignet)
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-#######################################################
+#
+#   Hint-peeking plugin
+#
+# This plugin allows peeking at some of the fields in a flashcard before
+# seeing the answer. This can be used to peek at example sentences,
+# pronunciation for Chinese/Japanese/Korean, etc.
+#
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from anki.hooks import wrap
 from ankiqt.ui import view
-from ankiqt.ui import main
 from anki.utils import hexifyID
 from ankiqt import mw
 
 # Settings
-CARD_MODEL="Recognition"
 HIDDEN_FIELD_INDEX=1
 SHOW_FIELD_KEY=Qt.Key_R
+CARD_TEMPLATE="Recognition"
 
-def newKeyPressEvent(self, evt):
+
+def newKeyPressEvent(evt):
     """Show answer on RET or register answer."""
-    if self.state == "showQuestion" and evt.key() == SHOW_FIELD_KEY:
-        if self.currentCard.cardModel.name == CARD_MODEL:
+    if mw.state == "showQuestion" and evt.key() == SHOW_FIELD_KEY:
+        if mw.currentCard.cardModel.name == CARD_TEMPLATE:
             evt.accept()
-            return self.moveToState("showHint")
+            return mw.moveToState("showHint")
+    return oldEventHandler(evt)
 
 def newOnLoadFinished(self):
-    #self.onLoadFinished()
+    self.onLoadFinished()
     if self.state == "showHint":
         mf = self.body.page().mainFrame()
         modelID = "cma" + hexifyID(self.main.currentCard.cardModel.id)
@@ -51,12 +51,14 @@ def newRedisplay(self):
         self.drawAnswer()
         self.flush()
 
-# We actually would like to use wrap method with pos="after", but wrap seems not to work with a method connected to a signal.
+oldEventHandler = mw.keyPressEvent
+mw.keyPressEvent = newKeyPressEvent
+view.View.redisplay = wrap(view.View.redisplay, newRedisplay, "after")
+# We actually would like to use wrap method with pos="after",
+# but wrap seems not to work with a method connected to a signal.
 # Hence the following hack, with new method calling old one.
 view.View.newOnLoadFinished = newOnLoadFinished
-mw.connect(mw.mainWin.mainText, SIGNAL("loadFinished(bool)"), mw.bodyView.newOnLoadFinished)
+mw.connect(mw.mainWin.mainText,
+           SIGNAL("loadFinished(bool)"), mw.bodyView.newOnLoadFinished)
 
-view.View.redisplay = wrap(view.View.redisplay, newRedisplay, "after")
-
-main.AnkiQt.keyPressEvent = wrap(main.AnkiQt.keyPressEvent, newKeyPressEvent, "before")
-
+mw.registerPlugin("Hint-peeking", 1)
